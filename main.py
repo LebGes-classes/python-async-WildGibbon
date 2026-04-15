194b9722d5e19dab9cc8bad2197a586cfbcae392
WildGibbon <chelovekebola@mail.ru>
2026-04-15 10:54:33 +0300

    finally im started to understand asyncs

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import asyncio
+import os
+import time
+
+import pandas as pd
 
 
-async def gay(text):
-    print(f"{text} gay started")
-    await asyncio.sleep(1)
-    print(f"{text} gay stopped")
+
+async def async_read_excel(path):
+    print(f"{path} gay started")
+    return await asyncio.to_thread(pd.read_excel, path)
 
 async def main():
-    await asyncio.gather(gay("1"), gay("2"))
+    coroutines = []
+
+    for file_name in os.listdir("data"):
+        if file_name.endswith(".xlsx"):
+            coroutines.append(async_read_excel("data/" + file_name))
+
+    results = await asyncio.gather(*coroutines)
+    print(results)
+
+
 
 asyncio.run(main())
