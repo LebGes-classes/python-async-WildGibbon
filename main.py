46d2d1dbd786862707ced1d326b95f986b414a18
WildGibbon <chelovekebola@mail.ru>
2026-04-16 09:21:03 +0300

    async i/o completed

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+import data_analytics.analizes_instruments as inst
+import pandas as pd
 import asyncio
 import os
-import time
-
-import pandas as pd
 
 
-
-async def async_read_excel(path):
-    print(f"{path} gay started")
+async def async_read_excel(path) -> pd.DataFrame:
     return await asyncio.to_thread(pd.read_excel, path)
 
+async def async_write_excel(sheets: list[pd.DataFrame], path) -> None:
+    with pd.ExcelWriter(path) as writer:
+        for i, sheet in enumerate(sheets):
+            await asyncio.to_thread(sheet.to_excel, writer, sheet_name=str(i))
+
 async def main():
-    coroutines = []
+    read_coroutines = []
+    write_coroutines = []
 
     for file_name in os.listdir("data"):
         if file_name.endswith(".xlsx"):
-            coroutines.append(async_read_excel("data/" + file_name))
+            read_coroutines.append(async_read_excel("data/" + file_name))
 
-    results = await asyncio.gather(*coroutines)
-    print(results)
+    data_frames : list[pd.DataFrame] = await asyncio.gather(*read_coroutines)
 
+    for i, df in enumerate(data_frames):
+        sheets = [inst.sort_by_calibration_dates(df),
+                  inst.filter_warranty(df),
+                  inst.sort_by_issues(df)]
+
+        write_coroutines.append(async_write_excel(sheets, f"output/output_{i}.xlsx"))
+
+    await asyncio.gather(*write_coroutines)
 
 
 asyncio.run(main())
